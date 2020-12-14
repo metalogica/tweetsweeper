5cd02698074152dc1099743391689ca90cca2b39
richard jarram <rjarram@me.com>
2020-12-13 20:59:43 -0500

    scaffolding unit tests

---
+++ src/components/Board.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+import Board from './Board'
+
+test('it renders in the DOM', () => {
+  render(<Board/>)
+})
+
+// Unit Tests
+it('draws the board', () => {
+
+})
---
+++ src/components/Cell.test.tsx
@@ -0,0 +1,6 @@
+import { render } from "@testing-library/react"
+import Cell from './Cell'
+
+test('default', () => {
+  render(<Cell/>)
+})
