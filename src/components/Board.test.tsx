b65338f889c65c277184ddce387fdd6400553bd3
richard jarram <rjarram@me.com>
2020-12-15 16:30:32 -0500

    scaffolded Board tests

--- src/components/Board.test.tsx
+++ src/components/Board.test.tsx
@@ -1,11 +1,51 @@
 import { render, screen } from '@testing-library/react';
 import Board from './Board'
+import { GameContext } from '../contexts'
 
 test('it renders in the DOM', () => {
   render(<Board/>)
 })
 
-// Unit Tests
-it('draws the board', () => {
+describe('buildBoard()', () => {
+  it('should build a new board and store it in state if it is a new game', () => {
 
+  })
+
+  it('should not build a new board if a game is in progress', () => {
+
+  })
+
+  it('should build a 5x5 grid with 1 mine for an Easy game.', () => {
+    // initialize game state
+
+    // render Board
+
+    // check the div structure of the board corresponds to 5x5 Cells
+  })
+
+  it('should build a 10x10 grid with 5 mines for an Regular game.', () => {
+    
+  })
+
+  it('should build a 20x20 grid with 10 mines for an Hard game.', () => {
+  
+  })
+})
+
+describe('drawBoard()', () => {
+  it('should render the entire revealed board when the game is over', () => {
+
+  })
+
+  it('should end the game if the user clicks on a mine', () => {
+
+  })
+
+  it('should end the game if the user flags all mines on the board', () => {
+
+  })
+
+  it('should recursively open all cells that are not mines or nieighbors of mines if a user clicks on a blank cell', () => {
+
+  })
 })
