0657d76386d1e7ec0311d91f307bb04213a92bf6
richard jarram <rjarram@me.com>
2020-12-17 16:32:19 -0500

    modified buildBoard() to accept `mineMap` for test case use

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -35,9 +35,10 @@ function buildBoard(
 ) 
 {
   if (numberOfMines > 0 && (mineMap[0][0] !== -1 || mineMap[0][1] !== -1 )) { 
-    throw new Error('Please EITHER set Random Mines via `numberOfMines` or use a `mineMap` to manually build mines on the grid')
+    throw new Error('Please EITHER set Random Mines via `numberOfMines` or use a `mineMap` to manually build mines on the grid.')
   }
   
+  // Build the grid and populate it with CellStates
   const grid: [any[], any[]] = [[],[]]
   let mineCount: number = 0
 
@@ -49,11 +50,13 @@ function buildBoard(
       // build new column
       if (grid[j][i] === undefined) grid[j].push([])
 
+      // set mines according to user defined `MineMap`; used specifically in test case in Board.test.tsx
+      const mine = mineMap.find(cell => cell[0] === j && cell[1] === i)
+
       const cellState: CellState = {
         location: [j, i],
         clicked: false,
-        // mine: randomMine(mineCount, numberOfMines),
-        mine: false,
+        mine: mine ? true : false,
         flagged: false,
         neighbors: 0
       }
@@ -62,6 +65,8 @@ function buildBoard(
     }
   }
 
+  // randomly select cells from grid to turn into mines
+  // will only run if no `mineMap` has been provided
   while (mineCount < numberOfMines) {
     let randomRow = Math.ceil(Math.random() * (grid.length - 1))
     let randomCol = Math.ceil(Math.random() * (grid[0].length - 1))
