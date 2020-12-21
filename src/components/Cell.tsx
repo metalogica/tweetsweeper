bb32c11dc8a6b6f14d8bb08aef9e5e9765ffb459
richard jarram <rjarram@me.com>
2020-12-21 07:25:13 -0500

    put cell style function back in for testing purposes

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,7 +1,8 @@
 import { CellState } from '../globals'
 import './Cell.scss'
 
-function Cell({ location, clicked, mine, flagged, neighbors, updateBoard, style }: CellState) {
+function Cell({ location, clicked, mine, flagged, neighbors, updateBoard }: CellState) {
+  const style: object = setStyle(location, clicked, mine, flagged, neighbors)
 
   return(
     <div  className='cell' 
@@ -11,6 +12,25 @@ function Cell({ location, clicked, mine, flagged, neighbors, updateBoard, style 
     >
     </div>
   )
+}
+
+function setStyle(location: [number, number], clicked: boolean, mine: boolean, flagged: boolean, neighbors: number) {
+  const skin = { 
+    backgroundImage: `url('/images/retro/unopened.svg')`,
+    gridArea: `${location[0]}-${location[1]}`
+  }
+
+  if (flagged) {
+    skin.backgroundImage = `url('/images/retro/flag.svg')`
+  } else if (clicked && mine) {
+    skin.backgroundImage = `url('/images/retro/mine.png')`
+  } else if (clicked && !mine && !flagged && neighbors === 0) {
+    skin.backgroundImage = `url('/images/retro/opened.svg')`
+  } else if (clicked && !mine && !flagged && neighbors > 0) {
+    skin.backgroundImage = `url('/images/retro/${neighbors}.svg')`
+  }
+
+  return skin;
 }
 
 export default Cell;
