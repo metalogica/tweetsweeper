bc7e6eac56a88c466cce4d651bcfc7665122aa33
richard jarram <rjarram@me.com>
2021-01-22 21:31:16 -0500

    rfactored completionmodal

--- src/components/ModalCompletion.test.tsx
+++ src/components/ModalCompletion.test.tsx
@@ -1,25 +1,52 @@
-import { render, screen, fireEvent, waitFor } from "@testing-library/react"
+import { render, screen, fireEvent } from "@testing-library/react"
 import '@testing-library/jest-dom/extend-expect'
-import ModalCompletion from './ModalCompletion'
+import App from '../App'
+import Board from './Board'
+import { testBoardState } from "../globals"
+
+async function failGame() {
+  const cell = screen.getByTestId('2-1')
+  fireEvent.click(cell)
+  return true
+}
+
+async function winGame() {
+  const mineCellOne = screen.getByTestId('2-1')
+  const mineCellTwo = screen.getByTestId('3-2')
+
+  fireEvent.contextMenu(mineCellOne)
+  fireEvent.contextMenu(mineCellTwo)
+
+  return true
+}
+
 
 describe('ModalCompletion', () => {
-  beforeEach(() => render(<ModalCompletion/>))
+  beforeEach(() => render(<App/>))
 
-  it('should not load when a game is present', () => {
+  it('should not load the modal when a game is ongoing', () => {
+    const modal = screen.queryByTestId("modal-completion")
+
+    expect(modal).toBeNull()
+  })
+
+  it('should load a Success message', async () => {
+    const gameWon = await winGame()
+    expect(gameWon).toBe(true)
+
     const modal = screen.getByTestId('modal-completion')
-
-    expect(modal).not.toBeInTheDocument()
+    expect(modal).toBeInTheDocument()
   })
+  
+  it('should load a Failure message', async () => {
+    const gameLost = await failGame()
+    expect(gameLost).toBe(true)
 
-  it('should load a Success message', () => {
-    const modal = screen.getByText("Congratulations! You trumped Trumped lies! Play Again?")
-
+    const modal = screen.getByTestId('modal-completion')
     expect(modal).toBeInTheDocument()
   })
 
-  it('should load a Failure message', () => {
-    const modal = screen.getByText("Dangit! Trump triumphed! Play again?")
+  it('should allow the user to restart the game', () => {
 
-    expect(modal).toBeInTheDocument()
   })
 })
--- src/components/ModalCompletion.tsx
+++ src/components/ModalCompletion.tsx
@@ -1,27 +1,26 @@
 import { useGameContext } from '../contexts'
 import { GameProgress } from '../globals'
 
+// end-game modal wrapper
 export default function ModalCompletion() {
   const { gameProgress } = useGameContext()
 
-
-
   return(
     <>
-      {
-        gameProgress === GameProgress.Won && (
-          <div data-testid='modal-completion'>
-            "Congratulations! You trumped Trumped lies! Play Again?"
-          </div>
-        )
-      }
-      {
-        gameProgress === GameProgress.Lost && (
-          <div data-testid='modal-completion'>
-            "Dangit! Trump triumphed! Play again?"
-          </div>
-        )
-      }
+      { gameProgress === GameProgress.Won && (<VictoryModal/>) }
+      { gameProgress === GameProgress.Lost && (<FailureModal/>) }
     </>
   )
 }
+
+const VictoryModal = () => (
+  <div data-testid='modal-completion'>
+    "Congratulations! You trumped Trump's lies! Play Again?"
+  </div>
+)
+
+const FailureModal = () => (
+  <div data-testid='modal-completion'>
+    "Dangit! Trump triumphed! Play again?"
+  </div>
+)
