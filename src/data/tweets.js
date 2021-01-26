c317970c07e7c787f0af8c1edcbafcffab661c1a
richard jarram <rjarram@me.com>
2021-01-25 21:16:18 -0500

    scaffolded tweet data

--- src/data/tweets.js
+++
@@ -1,6 +0,0 @@
-const trumpTweets = {
-  id: 1,
-  content: 'The tweet itself',
-  lie: false,
-  jutification: "Why it's true or false"
-}
---
+++ src/data/tweets.ts
@@ -0,0 +1,6 @@
+export interface Tweet {
+  id: number;
+  lie: boolean;
+  content: string;
+  background: string;
+}
