6ebd6c8817455a8f57c5c92e064a4ea670c85f0b
richard jarram <rjarram@me.com>
2021-02-03 18:13:55 -0500

    completed functionality on tweet hover

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,13 +1,19 @@
 import { CellState } from '../globals'
 
-export default function Tweet({ clicked, mine, flagged, tweet }: CellState) { 
+export default function Tweet({ clicked, tweet }: CellState) { 
   if (tweet) {
-    const { date, content } = tweet
+    const { date, content, lie, source } = tweet
     return(
       <>
         <div data-testid='tweet-panel'>
           <p data-testid='tweet-panel-date' className='date'>{date}</p>
           <p data-testid='tweet-panel-content' className='content'>{content}</p>  
+          { clicked && (
+            <>
+              <p>Status : {lie ? 'Lie!' : 'True'} </p>
+              <p>Source : {source} </p>
+            </>
+          ) }
         </div>
       </>
     )
--- src/components/dist/Tweet.js
+++ src/components/dist/Tweet.js
@@ -1,13 +1,22 @@
 "use strict";
 exports.__esModule = true;
 function Tweet(_a) {
-    var clicked = _a.clicked, mine = _a.mine, flagged = _a.flagged, tweet = _a.tweet;
+    var clicked = _a.clicked, tweet = _a.tweet;
     if (tweet) {
-        var date = tweet.date, content = tweet.content;
+        var date = tweet.date, content = tweet.content, lie = tweet.lie, source = tweet.source;
         return (React.createElement(React.Fragment, null,
             React.createElement("div", { "data-testid": 'tweet-panel' },
                 React.createElement("p", { "data-testid": 'tweet-panel-date', className: 'date' }, date),
-                React.createElement("p", { "data-testid": 'tweet-panel-content', className: 'content' }, content))));
+                React.createElement("p", { "data-testid": 'tweet-panel-content', className: 'content' }, content),
+                clicked && (React.createElement(React.Fragment, null,
+                    React.createElement("p", null,
+                        "Status : ",
+                        lie ? 'Lie!' : 'True',
+                        " "),
+                    React.createElement("p", null,
+                        "Source : ",
+                        source,
+                        " "))))));
     }
     else {
         return (React.createElement(React.Fragment, null));
