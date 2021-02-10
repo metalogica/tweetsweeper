28a299bc1efc3097f800b903821828d947a76c51
richard jarram <rjarram@me.com>
2021-02-10 13:28:33 -0500

    added paperclip mascot

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,43 +1,55 @@
 import ModalCompletion from './ModalCompletion'
+import { useGameContext } from '../contexts'
 import { CellState } from '../globals'
 import './Tweet.scss'
-
-const helpText = `
-The Trump Era has ended but his Tweets still wreck havoc in a post-truth world.\n
-Be a hero and de-contaminate the falsehoods that pollute contemporary political discourse.\n
-Simply right-click on the cells you think contain explosive lies.\n
-Click on the Start menu below for further options.
-`
 
 const formatTweetText = (tweetContent: string) => `
   ${tweetContent.slice(0,200)}${tweetContent.length > 200 ? '...' : ''}
 `
 
-const Tweet = ({ clicked, tweet } : CellState) => (
-  <div className="tweet-wrapper">
-    <div className='tweet-container' data-testid='tweet-panel'>
+const Tweet = ({ clicked, tweet } : CellState) => {
+  const { gameProgress } = useGameContext()
+
+  const helpText = `
+  The Trump Era has ended but his Tweets still wreck havoc in a post-truth world.\n
+  Be a hero and de-contaminate the falsehoods that pollute contemporary political discourse.\n
+  Simply right-click on the cells you think contain explosive lies.\n
+  Click on the Start menu below for further options.
+`
+
+  const tweetRibbon = () => (
+    <>
       <span className='tweet-ribbon'>{tweet ? 'Tweet Panel' : 'Help'}</span>
-      <span className='tweet-logo'></span>
-      <div className='tweet-content'>
-        {
-          !tweet && <div className='tweet-help-dialogue'>{helpText}</div>
-        }
-        { 
-          tweet && (
-            <>
-              <span className='date' data-testid='tweet-panel-date'>{tweet.date}</span>
-              <p className='content' data-testid='tweet-panel-content' >{formatTweetText(tweet.content)}</p>  
-              { clicked && (
-                <>
-                  <span>Status: <span className={tweet.lie ? 'lie' : 'truth'}>{tweet.lie ? 'Lie!' : 'True'}</span></span>
-                  {/* <p className='source' >Source : {tweet.source} </p> */}
-                </>
-              )}
-            </>
-          )}
+      <span className={tweet ? 'tweet-logo' : 'help-logo'}></span>
+    </>
+  )
+  
+  return (
+    <div className="tweet-wrapper">
+      <div className='tweet-container' data-testid='tweet-panel'>
+        {tweetRibbon()}
+        <div className='tweet-content'>
+          {
+            !tweet && <div className='tweet-help-dialogue'>{helpText}</div>
+          }
+          { 
+            tweet && (
+              <>
+                <span className='date' data-testid='tweet-panel-date'>{tweet.date}</span>
+                <p className='content' data-testid='tweet-panel-content' >{formatTweetText(tweet.content)}</p>  
+                { clicked && (
+                  <>
+                    <span>Status: <span className={tweet.lie ? 'lie' : 'truth'}>{tweet.lie ? 'Lie!' : 'True'}</span></span>
+                    {/* <p className='source' >Source : {tweet.source} </p> */}
+                  </>
+                )}
+              </>
+            )
+          }
+        </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Tweet;
--- src/components/dist/Tweet.js
+++ src/components/dist/Tweet.js
@@ -1,14 +1,18 @@
 "use strict";
 exports.__esModule = true;
+var contexts_1 = require("../contexts");
 require("./Tweet.scss");
-var helpText = "\nThe Trump Era has ended but his Tweets still wreck havoc in a post-truth world.\n\nBe a hero and de-contaminate the falsehoods that pollute contemporary political discourse.\n\nSimply right-click on the cells you think contain explosive lies.\n\nClick on the Start menu below for further options.\n";
 var formatTweetText = function (tweetContent) { return "\n  " + tweetContent.slice(0, 200) + (tweetContent.length > 200 ? '...' : '') + "\n"; };
 var Tweet = function (_a) {
     var clicked = _a.clicked, tweet = _a.tweet;
+    var gameProgress = contexts_1.useGameContext().gameProgress;
+    var helpText = "\n  The Trump Era has ended but his Tweets still wreck havoc in a post-truth world.\n\n  Be a hero and de-contaminate the falsehoods that pollute contemporary political discourse.\n\n  Simply right-click on the cells you think contain explosive lies.\n\n  Click on the Start menu below for further options.\n";
+    var tweetRibbon = function () { return (React.createElement(React.Fragment, null,
+        React.createElement("span", { className: 'tweet-ribbon' }, tweet ? 'Tweet Panel' : 'Help'),
+        React.createElement("span", { className: tweet ? 'tweet-logo' : 'help-logo' }))); };
     return (React.createElement("div", { className: "tweet-wrapper" },
         React.createElement("div", { className: 'tweet-container', "data-testid": 'tweet-panel' },
-            React.createElement("span", { className: 'tweet-ribbon' }, tweet ? 'Tweet Panel' : 'Help'),
-            React.createElement("span", { className: 'tweet-logo' }),
+            tweetRibbon(),
             React.createElement("div", { className: 'tweet-content' },
                 !tweet && React.createElement("div", { className: 'tweet-help-dialogue' }, helpText),
                 tweet && (React.createElement(React.Fragment, null,
